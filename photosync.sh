87bf5e7d09862fe88938864a64224c1c4af0419a
Stian Thaulow <stian@stianthaulow.com>
2014-11-30 22:14:28 +0100

    Adds iPhoto rsync script

---
+++ photosync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Killing iPhoto and Aperture..."
+killall iPhoto
+killall Aperture
+ssh Stian@thaulow.ddns.net killall iPhoto && killall Aperture
+echo "Syncing..."
+rsync -a --info=progress2 --delete /Volumes/Rikiki/Pictures/Clean.aplibrary/ Stian@thaulow.ddns.net:~/Pictures/Clean.aplibrary
+echo "Done."
